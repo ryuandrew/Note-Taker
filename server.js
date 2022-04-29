ec9c449a4ce71138d174de364cb76e9e9b9a3428
Andrew Ryu <andrewryu@outlook.com>
2022-04-28 17:27:51 -0700

    updated readme

--- server.js
+++ server.js
@@ -13,15 +13,6 @@ app.use(express.urlencoded({extended: true}))
 app.use(express.static('public'))
 app.use(routes)
 
-
-// GET /api/notes should read the db.json file and return all saved notes as JSON.
-// app.route("")
-
-
-// POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
-
-// npm uuid
-
 // console logs in node
 app.listen(PORT, () => 
     console.log(`App listening at http://localhost:${PORT}`)
